c68cf65b2d9c66bef248afff4670451ac3de6154
VitorOSZz <harlerquin224@gmail.com>
2026-01-09 14:59:20 -0300

    added theme,  favicon, semantic htm . Correct on media queries

--- desafios/desafio 016/assets/scripts/script.js
+++ desafios/desafio 016/assets/scripts/script.js
@@ -1,36 +1,48 @@
-const botao = document.getElementById('botao-tema');
+button = document.body.querySelector('#dark-mode')
+themeDark = false
 const body = document.body;
 
-// Persistência do tema
-const temasalvo = localStorage.getItem('tema');
-temaEscuro(temasalvo === 'escuro');
+// When reenter the website return the old theme
+const savedTheme = localStorage.getItem('theme');
+darkTheme(savedTheme === 'dark');
 
-// Função para alternar entre tema claro e escuro
-function temaEscuro(tipo) {
-  if (tipo == true) {
-    body.classList.add('escuro');
-    botao.innerHTML = '<i class="fa-solid fa-sun"></i>';
+console.log('Saved theme ' + savedTheme)
+
+// Function to change the theme
+function darkTheme(themeDark) {
+  
+  console.log('darkTheme executed')
+
+  if (themeDark == true) {
+
+    body.classList.add('dark')
+
+    themeDark = true
   } else {
-    body.classList.remove('escuro');
-    botao.innerHTML = '<i class="fa-solid fa-moon"></i>';
+
+    body.classList.remove('dark')
+    themeDark = false
   }
+  console.log('Theme now is: ' + (themeDark ? 'Dark' : 'Light'))
 }
 
-botao.addEventListener('click', () => {
-  const isescuro = body.classList.toggle('escuro');
-  temaEscuro(isescuro);
-  localStorage.setItem('tema', isescuro ? 'escuro' : 'claro');
+button.addEventListener('click', () => {
+  const isdark = body.classList.toggle('dark');
+  darkTheme(isdark);
+  localStorage.setItem('theme', isdark ? 'dark' : 'light');
+  console.log('Button event executed ' + isdark)
 });
 
-// Scroll suave para links de navegação
-const navLinks = document.querySelectorAll('#menu ul a.link');
+// Low Scroll on header
+const navLinks = document.querySelectorAll('header nav ul a.link');
 navLinks.forEach(link => {
   link.addEventListener('click', function(e) {
+    console.log('link clicked')
     e.preventDefault();
     const target = document.querySelector(this.getAttribute('href'));
     if (target) {
       const headerHeight = document.querySelector('header').offsetHeight;
-      const targetPosition = target.offsetTop - headerHeight - 20;
+      const targetPosition = target.offsetTop - headerHeight - 50;
       window.scrollTo({
         top: targetPosition,
         behavior: 'smooth'
